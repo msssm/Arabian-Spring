2c6b61bb4640c0ad1f5c3bf5f394fca4c058526d
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-10-24 23:42:33 +0200

    modified README, added doc base structure, added .gitignore

---
+++ doc/auto/header.el
@@ -0,0 +1,68 @@
+(TeX-add-style-hook "header"
+ (lambda ()
+    (LaTeX-add-environments
+     "mydef"
+     "mythm")
+    (TeX-add-symbols
+     '("units" 1)
+     '("norm" 1)
+     '("abs" 1)
+     '("mat" 1)
+     '("bvec" 1)
+     '("pdiff" 1)
+     '("sdiff" 1)
+     '("odiff" 1)
+     '("iztrans" 1)
+     '("ztrans" 1)
+     '("ifour" 1)
+     '("four" 1)
+     '("ilap" 1)
+     '("lap" 1)
+     "iu"
+     "ju"
+     "cel"
+     "degree"
+     "matlab"
+     "fort"
+     "eg"
+     "ie")
+    (TeX-run-style-hooks
+     "appendix"
+     "setspace"
+     "pict2e"
+     "slashbox"
+     "multirow"
+     "calc"
+     "url"
+     "nomencl"
+     "xspace"
+     "subfig"
+     "float"
+     "fancyvrb"
+     "afterpage"
+     "psfrag"
+     "xcolor"
+     "graphicx"
+     "dvips"
+     "fancyhdr"
+     "cite"
+     "caption"
+     "sf}"
+     "latexsym"
+     "bm"
+     "mathrsfs"
+     "amsfonts"
+     "amsthm"
+     "amssymb"
+     "amsmath"
+     "geometry"
+     "23cm}"
+     "centering"
+     "a4"
+     "fontenc"
+     "T1"
+     "babel"
+     "english"
+     "inputenc"
+     "utf8x")))
+
---
+++ doc/auto/master.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-add-symbols
+     '("nomunit" 1)
+     "Top"
+     "Bot"
+     "bibdir"
+     "bibtitle")
+    (TeX-run-style-hooks
+     "latex2e"
+     "art11"
+     "article"
+     "11pt"
+     "a4paper"
+     "header"
+     "./titlepage/cover")))
+
