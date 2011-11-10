f4d38522f367c6db5d334e02bb2f556ab71b2dc3
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-11-10 21:45:49 +0100

    added main.m script and solver function

---
+++ src/func/solverSIR.m
@@ -0,0 +1,23 @@
+% --------------------------------*- Matlab -*---------------------------------
+% Filename: solverSIR.m
+% Description: Solver function that employs the SIR model for opinion
+%              spreading.  The function takes a list of agent structures and a
+%              parameter structure, which is defined in main.m script.  The
+%              function generates an output structure for post-processing.
+% Author: Fabian Wermelinger
+% Email: fabianw@student.ethz.ch
+% Created: Thu Nov 10 21:29:23 2011 (+0100)
+% Version: 
+% Last-Updated: Thu Nov 10 21:42:50 2011 (+0100)
+%           By: Fabian Wermelinger
+%     Update #: 5
+% -----------------------------------------------------------------------------
+% solverSIR.m starts here
+% -----------------------------------------------------------------------------
+function res = solverSIR( agent, par )
+% Solve the opinion formation problem using a SIR model.
+    
+
+% -----------------------------------------------------------------------------
+% solverSIR.m ends here
+% -----------------------------------------------------------------------------
