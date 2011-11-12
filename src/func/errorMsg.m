a138938046ca34cc59797a0b6beb957e7a021a4b
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-11-12 18:45:40 +0100

    just found an error, minor-> ignore this commit

--- src/func/errorMsg.m
+++ src/func/errorMsg.m
@@ -7,21 +7,21 @@
 % Email: fabianw@student.ethz.ch
 % Created: Sat Nov 12 16:47:45 2011 (+0100)
 % Version: 
-% Last-Updated: Sat Nov 12 18:13:56 2011 (+0100)
+% Last-Updated: Sat Nov 12 18:44:22 2011 (+0100)
 %           By: Fabian Wermelinger
-%     Update #: 2
+%     Update #: 3
 % -----------------------------------------------------------------------------
 % errorMsg.m starts here
 % -----------------------------------------------------------------------------
 function err = errorMsg( ID, stack )
 % create error message structure for use with the error( err ) function.
-    switch ( lower(stack.name) )
+    switch ( lower(stack(1).name) )
       case ( 'solversir' ) % errors associated with the solverSIR.m function
         switch ( lower(ID) )
           case ( 'wrongnumberofnodes' )
             err.message = ['number of global nodes does not match the ' ...
                            'length of the agent list'];
-            err.identifier = [stack.name ':agentList'];
+            err.identifier = [stack(1).name ':agentList'];
             err.stack = stack;
         end
         
