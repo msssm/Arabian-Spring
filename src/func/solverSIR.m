271f3b7250420398482cae70f23085e16ae7a63b
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-11-19 18:23:03 +0100

    solverSIR is working on a first stage

--- src/func/solverSIR.m
+++ src/func/solverSIR.m
@@ -8,13 +8,13 @@
 % Email: fabianw@student.ethz.ch
 % Created: Thu Nov 10 21:29:23 2011 (+0100)
 % Version: 
-% Last-Updated: Sat Nov 12 18:29:59 2011 (+0100)
+% Last-Updated: Sat Nov 19 18:15:20 2011 (+0100)
 %           By: Fabian Wermelinger
-%     Update #: 7
+%     Update #: 34
 % -----------------------------------------------------------------------------
 % solverSIR.m starts here
 % -----------------------------------------------------------------------------
-function res = solverSIR( agent, par )
+function result = solverSIR( agent, par )
 % Solve the opinion formation problem using a SIR model.
     if ( length(agent) ~= sum(par.nodes) )
         [stack, I] = dbstack( '-completenames' );
@@ -22,31 +22,73 @@ function res = solverSIR( agent, par )
     end
     
     % create time vector
-    t = linspace( pat.time(1), par.time(2), par.nTime );
+    t = linspace( par.time(1), par.time(2), par.nTime );
 
+    % init residual vector(s)
+    res = zeros( length(t), length(par.nodes) + 1 ); % +1 is for global res
+    
     % start iteration
     for i = 1:length( t )
+        % residual
+        res(i,:) = calcResidual( agent, par );
         for j = 1:length( agent )
             for k = 1:length( agent(j).nbr )
-                if ( agent(j).citizen == agent(k).citizen )
-                    % agent j and agent k are citizens of the same counrty
-                    % (i.e. cluster) and know each other
-                    if ( rand() < par.beta )
-                        agent(k) = setStateNative( agent(j), agent(k) );
-                    end
-                else
-                    % agent j and agent k are not citizens of the same
-                    % country (i.e. cluster) and know each other
-                    if ( rand() < par.beta )
-                        agent(k) = setStateAlien( agent(j), agent(k) );
-                    end
-                end
+                % agent(j) gets influenced by its neighbor agent(k)
+                agent(j) = applyInfluence( ...
+                    agent(k), agent(j), res(i, par.riotOrigin), par );
             end
         end
-        % residual
-        
     end
-    res = 1;
+    result = res;
+    return;
+    
+% -----------------------------------------------------------------------------
+% subroutines
+% -----------------------------------------------------------------------------
+function b = applyInfluence( a, b, res, par )
+% apply the influence of agent a on agent b.  If a and b are not both
+% residents of the par.riotOrigin country (i.e. cluster number), the the
+% actual residual of the par.riotOrigin country decides wether a influences
+% b at the actual timestep.  The parameter par.riotOriginThreshold defines
+% the limit for inter-cluster influence
+    bTmp.motivated = false;
+    if ( res <= par.riotOriginThreshold )
+        if ( a.citizen == b.citizen && a.citizen == par.riotOrigin )
+            % agent j and agent k are citizens of the same counrty
+            % (i.e. cluster) and know each other
+            if ( rand() < par.beta )
+                bTmp.state = b.state;
+                bTmp.motivated = true;
+                b = setStateNative( a, b );
+            end
+        end
+    else
+        if ( a.citizen == b.citizen )
+            % agent j and agent k are citizens of the same counrty
+            % (i.e. cluster) and know each other
+            if ( rand() < par.beta )
+                bTmp.state = b.state;
+                bTmp.motivated = true;
+                b = setStateNative( a, b );
+            end
+        else
+            % agent j and agent k are not citizens of the same
+            % country (i.e. cluster) and know each other
+            if ( rand() < par.beta )
+                bTmp.state = b.state;
+                bTmp.motivated = true;
+                b = setStateAlien( a, b );
+            end
+        end
+    end
+    if ( bTmp.motivated )
+        % if b was motivated by a to change its mind, there is probability
+        % par.gamma that b was not sure about this new thinking and returns
+        % to its old  mindstate instead
+        if ( rand() < par.gamma )
+            b.state = bTmp.state;
+        end
+    end
     return;
     
 function b = setStateNative( a, b )
@@ -58,7 +100,40 @@ function b = setStateAlien( a, b )
 % set the mind-state of alien agent b under influence of agent a
     b = setStateNative( a, b );
     return;
-                    
+
+function res = calcResidual( agent, par )
+% calculate residual for each cluster as well as global
+    res = cell( 1, length(par.nodes) + 1 );
+    for i = 1:length( par.nodes )
+        res{i} = zeros( par.nodes(i), 1 );
+    end
+    res{length(res)} = zeros( length(agent), 1 );
+
+    % init array addressing
+    counter = ones( size(res) );
+    for i = 1:length( agent )
+        % local residual vector entry for agent(i)
+        res{agent(i).citizen}(counter(agent(i).citizen)) = agent(i).state;
+        counter(agent(i).citizen) = counter(agent(i).citizen) + 1;
+        % global residual entry for agent(i)
+        res{length(res)}(counter(length(counter))) = agent(i).state;
+        counter(length(counter)) = counter(length(counter)) + 1;
+    end
+    % calculate maxRes for normalization
+    maxRes = zeros( size(res) );
+    for i = 1:length( par.nodes )
+        maxRes(i) = norm( ones(par.nodes(i), 1) ); % local
+    end
+    maxRes(length(maxRes)) = norm( ones(sum(par.nodes), 1) ); % global
+
+    % normalize
+    tmpRes = zeros( size(res) );
+    for i = 1:length( res )
+        tmpRes(i) = norm( res{i} )/maxRes(i);
+    end
+    res = tmpRes;
+    return;
+    
 % -----------------------------------------------------------------------------
 % solverSIR.m ends here
 % -----------------------------------------------------------------------------
--- src/main/main.m
+++ src/main/main.m
@@ -5,9 +5,9 @@
 % Email: fabianw@student.ethz.ch
 % Created: Thu Nov 10 21:20:16 2011 (+0100)
 % Version: 
-% Last-Updated: Sat Nov 12 18:39:07 2011 (+0100)
+% Last-Updated: Sat Nov 19 18:22:06 2011 (+0100)
 %           By: Fabian Wermelinger
-%     Update #: 5
+%     Update #: 27
 % -----------------------------------------------------------------------------
 % main.m starts here
 % -----------------------------------------------------------------------------
@@ -37,19 +37,48 @@ par.nodes = [100 150 200];
 % two element vector [tStart tEnd].  the nTime variable defines the number
 % of nodes in the time domain.
 par.time = [0 60]; % [day]
-par.nTime = 1000;
+par.nTime = 600;
 
 % the beta and gamma variables define the infection rate and the immunity
 % rate, respectively, of the SIR model.
-par.beta = 0.001; % [day^-1]
-par.gamma = 0.00001; % [day^-1]
+par.beta = 0.005; % [day^-1]
+par.gamma = 0.0008; % [day^-1]
+
+% the riotOrigin parameter defines the number of the cluster in which the
+% uprise against the government starts.  the riotOriginThreshold is a scalar
+% between 0 and 1 and describes the point at which the riots in riotOrigin
+% spreads over to the other clusters.  0 means that it immediately spreads
+% over, 1 means that it spreads when riotOrigin already is in maximum
+% revolution.
+par.riotOrigin = 1;
+par.riotOriginThreshold = 0.6;
 
 % -----------------------------------------------------------------------------
 % start simulation
 % -----------------------------------------------------------------------------
 
+% test solverSIR.m
+n = 1;
+N = sum( par.nodes );
+imin = [1 par.nodes(2) par.nodes(3)];
+imax = [par.nodes(1) sum(par.nodes(1:2)) sum(par.nodes)];
 
+for i = 1:length( par.nodes )
+    for j = 1:par.nodes(i)
+        agent(n).state = rand();
+        dummy = [1 randi(8)];
+        agent(n).nbr = randi( N, dummy );
+        % agent(n).nbr = randi( [imin(i) imax(i)] , dummy );
+        agent(n).alpha = 0.6;
+        agent(n).citizen = i;
+        n = n+1;
+    end
+end
+
+res = solverSIR( agent, par );
+plot( res );
 
 % -----------------------------------------------------------------------------
 % main.m ends here
 % -----------------------------------------------------------------------------
+
