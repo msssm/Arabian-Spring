a163ea2361cad43dbf0b1a2908e6a3256296d346
tconring <tconring@gmail.com>
2011-11-23 16:29:54 +0100

    smallworld.m

--- src/func/smallworld.m
+++ src/func/smallworld.m
@@ -13,7 +13,7 @@
 % http://www.soms.ethz.ch/matlab
 % Authors: Stefan Brugger and Cristoph Schwirzer, 2011
 
-function A = small_world(par)
+function A = smallworld(par)
 % Generate a small world graph using the "Watts and Strogatz model" as
 % described in Watts, D.J.; Strogatz, S.H.: "Collective dynamics of
 % 'small-world' networks."
@@ -30,25 +30,25 @@ function A = small_world(par)
 
 % Construct a regular lattice: a graph with n nodes, each connected to k
 % neighbors, k/2 on each side.
-k = par.kHalf*2;
-rows = reshape(repmat([1:par.nodes]', 1, k), par.nodes*k, 1);
-columns = rows+reshape(repmat([[1:par.kHalf] [par.nodes-par.kHalf:n-1]], par.nodes, 1), par.nodes*k, 1);
-columns = mod(columns-1, par.nodes) + 1;
-B = sparse(rows, columns, ones(par.nodes*k, 1));
-A = sparse([], [], [], par.nodes, par.nodes);
+k = par.kHalf(i)*2;
+rows = reshape(repmat([1:par.nodes(i)]', 1, k), par.nodes(i)*k, 1);
+columns = rows+reshape(repmat([[1:par.kHalf(i)] [par.nodes(i)-par.kHalf(i):n-1]], par.nodes(i), 1), par.nodes(i)*k, 1);
+columns = mod(columns-1, par.nodes(i)) + 1;
+B = sparse(rows, columns, ones(par.nodes(i)*k, 1));
+A = sparse([], [], [], par.nodes(i), par.nodes(i));
 
 % With probability beta rewire an edge avoiding loops and link duplication.
 % Until step i, only the columns 1:i are generated making implicit use of A's
 % symmetry.
-for i = [1:par.nodes]
+for i = [1:par.nodes(i)]
     % The i-th column is stored full for fast access inside the following loop.
     col= [full(A(i, 1:i-1))'; full(B(i:end, i))];
     for j = i+find(col(i+1:end))'
-        if (rand()<par.alpha)
+        if (rand()<par.alpha(i))
             col(j)=0;
-            k = randi(par.nodes);
+            k = randi(par.nodes(i));
             while k==i || col(k)==1
-                k = randi(par.nodes);
+                k = randi(par.nodes(i));
             end
             col(k) = 1;
         end
