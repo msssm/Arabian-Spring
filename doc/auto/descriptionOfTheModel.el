7543cdfa15b618d090c62c27b7c4d4bd9868c9c6
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-12-12 19:18:00 +0100

    updated doc structure

---
+++ doc/auto/descriptionOfTheModel.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "descriptionOfTheModel"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:descriptionOfTheModel")
+    (TeX-run-style-hooks
+     "descriptionKuran"
+     "descriptionNetworks"
+     "descriptionSolver")))
+
