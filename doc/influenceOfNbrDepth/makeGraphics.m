9458795a7895b58e3ea9e673bbfd191c22e034c7
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-12-14 16:38:36 +0100

    new influenceOfNbrDepth.pdf plot

--- doc/influenceOfNbrDepth/makeGraphics.m
+++ doc/influenceOfNbrDepth/makeGraphics.m
@@ -5,9 +5,9 @@
 % Email: fabianw@student.ethz.ch
 % Created: Wed Dec 14 14:18:12 2011 (+0100)
 % Version: 
-% Last-Updated: Wed Dec 14 15:32:15 2011 (+0100)
+% Last-Updated: Wed Dec 14 16:35:08 2011 (+0100)
 %           By: Fabian Wermelinger
-%     Update #: 24
+%     Update #: 27
 % -----------------------------------------------------------------------------
 % makeGraphics.m starts here
 % -----------------------------------------------------------------------------
@@ -20,6 +20,12 @@ resData{1} = res;
 load( 'nbrDepth2.mat' );
 resData{2} = res;
 
+load( 'nbrDepth1_random.mat' );
+resData{3} = res;
+
+load( 'nbrDepth2_random.mat' );
+resData{4} = res;
+
 fig(1) = figure;
 ax(1) = axes;
 fig(2) = figure;
@@ -27,30 +33,30 @@ ax(2) = axes;
 hold( ax(1), 'on' );
 hold( ax(2), 'on' );
 
-lineStyle = { '-k' '--k' '-.k' };
-legendStr = { 'nbrDepth = 1' 'nbrDepth = 2' '$8\;\%$' };
+lineStyle = { '-k' '--k' };
+legendStr = { 'Small World' 'Random' };
 
 % -----------------------------------------------------------------------------
 % fig1
-for i = 1:length( resData )
-    h(i) = plot( ax(1), resData{i}(:,1), lineStyle{i} );
-end
+h(1) = plot( ax(1), resData{1}(:,4), lineStyle{1} );
+h(2) = plot( ax(1), resData{3}(:,4), lineStyle{2} );
+
 xlabel( ax(1), 'Number of Iterations' )
-ylabel( ax(1), 'Residual' )
+ylabel( ax(1), 'Global Residual' )
 legend( h, legendStr, 'location', 'se', 'FontSize', 14 );
-title( ax(1), 'Cluster 1' );
+title( ax(1), 'nbrDepth = 1' );
 set( ax(1), 'YLim', [0 1] );
 grid( ax(1), 'on' );
 
 % -----------------------------------------------------------------------------
 % fig2
-for i = 1:length( resData )
-    h(i) = plot( ax(2), resData{i}(:,4), lineStyle{i} );
-end
+h(1) = plot( ax(2), resData{2}(:,4), lineStyle{1} );
+h(2) = plot( ax(2), resData{4}(:,4), lineStyle{2} );
+
 xlabel( ax(2), 'Number of Iterations' )
-ylabel( ax(2), 'Residual' )
+ylabel( ax(2), 'Global Residual' )
 legend( h, legendStr, 'location', 'se', 'FontSize', 14 );
-title( ax(2), 'Global' );
+title( ax(2), 'nbrDepth = 2' );
 set( ax(2), 'YLim', [0 1] );
 grid( ax(2), 'on' );
 
