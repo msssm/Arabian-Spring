2e425a6ee77b04db61fde80a987616ba6ea38990
Fabian Wermelinger <fabianw@student.ethz.ch>
2011-12-15 21:54:32 +0100

    corrected some typos

---
+++ doc/auto/descriptionNetworks.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook "descriptionNetworks"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:descriptionofnetwork"
+     "influencerewiringprob"
+     "sec:smallworld"
+     "sec:randomgraph")))
+
---
+++ doc/auto/implementNetworks.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "implementNetworks"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:implementationofnetworks")))
+
