0d78c967a4b9f9903a8ab5f656082685f4fea4a5
Fab Wermelinger <fabian.wermelinger@hslu.ch>
2011-12-15 12:12:38 +0100

    added Sebastians figure

---
+++ doc/influenceOffixedthresholds/auto/minimal.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "minimal"
+ (lambda ()
+    (TeX-run-style-hooks
+     "color"
+     "graphicx"
+     "dvips"
+     "psfrag"
+     "latex2e"
+     "minimal10"
+     "fixedthreshold1"
+     "fixedthreshold2")))
+
